e9319b2a3b456441f99f7dfbffd68634915851b8
samcrow <sam.crow@liquidr.com>
2013-08-23 08:33:25 -0700

    Fixed marked ant recording experiment class (cherry picked from commit 3661442500c6c28379eedcf781146ece1d543c8f)

--- src/main/java/org/samcrow/colonydata/experiments/MarkedAntRecordingExperiment.java
+++ src/main/java/org/samcrow/colonydata/experiments/MarkedAntRecordingExperiment.java
@@ -5,13 +5,17 @@
 package org.samcrow.colonydata.experiments;
 
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Transient;
 
 /**
  * An experiment in which events with marked ants were recorded
  * @author Sam Crow
  */
+@Entity
 public class MarkedAntRecordingExperiment extends Experiment {
 
+    @Transient
     @Override
     public List<? extends ExperimentResult> getResults() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
